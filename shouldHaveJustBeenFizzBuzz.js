028c4aa79e0cac65055195020c549fae4bcac51e
John Fiewor <johnfiewor@gmail.com>
2021-05-20 10:43:41 +0100

    removed slice. used push to achieve correct result

--- shouldHaveJustBeenFizzBuzz.js
+++ shouldHaveJustBeenFizzBuzz.js
@@ -2,21 +2,27 @@ let arr = [];
 function checkYuGiOh(n) {
     for(let i = 1; i <= n; i++) {
         if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
-            arr.splice(arr.indexOf(i), 1, "yu-gi-oh");            
+            arr.push("yu-gi-oh");
+        } else if (i % 2 === 0 && i % 5 === 0) {
+            arr.push("yu-oh");
         } else if (i % 2 === 0 && i % 3 === 0) {
-            // select array element at index i
-            // use splice to replace it with text
-            arr.splice(arr.indexOf(i), 1, "yu-gi");
+            arr.push("yu-gi");
         } else if (i % 2 === 0) {
-            arr.splice(arr.indexOf(i), 1, "yu");
+            arr.push("yu");
         } else if (i % 3 === 0) {
-            arr.splice(arr.indexOf(i), 1, "gi");
+            arr.push("gi");
         }  else if (i % 5 === 0) {
-            arr.splice(arr.indexOf(i), 1, "oh");
+            arr.push("oh");
+        } else if (typeof i === 'string') {
+            return i;
+        } else {
+            arr.push(i);
         }
-        arr.push(i);
     }
     return arr;
 }
+// select array element at index i
+// use splice to replace it with text
+//arr.splice(arr.indexOf(i), 1, "yu-gi");
 
-console.log(checkYuGiOh(10));
+console.log(checkYuGiOh("fizzbuzz is meh"));
