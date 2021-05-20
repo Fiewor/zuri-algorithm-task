f95bd4e990d26b26a0d256d2aab8f9ece7f682f0
John Fiewor <johnfiewor@gmail.com>
2021-05-20 10:54:22 +0100

    closes #1

--- shouldHaveJustBeenFizzBuzz.js
+++ shouldHaveJustBeenFizzBuzz.js
@@ -1,25 +1,27 @@
 let arr = [];
 function checkYuGiOh(n) {
-    for(let i = 1; i <= n; i++) {
-        if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
-            arr.push("yu-gi-oh");
-        } else if (i % 2 === 0 && i % 5 === 0) {
-            arr.push("yu-oh");
-        } else if (i % 2 === 0 && i % 3 === 0) {
-            arr.push("yu-gi");
-        } else if (i % 2 === 0) {
-            arr.push("yu");
-        } else if (i % 3 === 0) {
-            arr.push("gi");
-        }  else if (i % 5 === 0) {
-            arr.push("oh");
-        } else if (typeof i === 'string') {
-            return i;
-        } else {
-            arr.push(i);
+    if (typeof n === 'string') {
+        return `invalid parameter: ${n}`;
+    } else {
+        for(let i = 1; i <= n; i++) {
+            if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+                arr.push("yu-gi-oh");
+            } else if (i % 2 === 0 && i % 5 === 0) {
+                arr.push("yu-oh");
+            } else if (i % 2 === 0 && i % 3 === 0) {
+                arr.push("yu-gi");
+            } else if (i % 2 === 0) {
+                arr.push("yu");
+            } else if (i % 3 === 0) {
+                arr.push("gi");
+            }  else if (i % 5 === 0) {
+                arr.push("oh");
+            } else {
+                arr.push(i);
+            }
         }
+        return arr;
     }
-    return arr;
 }
 // select array element at index i
 // use splice to replace it with text
