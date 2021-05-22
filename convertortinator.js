65248a6c0253615c5b4696b81325acd3df3733b7
Fiewor John <johnfiewor@gmail.com>
2021-05-22 21:43:11 +0100

    Update convertortinator.js

--- convertortinator.js
+++ convertortinator.js
@@ -1,6 +1,6 @@
 let type;
 function convertFahrToCelsius (fahr) {
-   let celsius = (fahr * 32) * 5/9;
+   let celsius = (fahr - 32) * 5/9;
    if (Array.isArray(fahr)) {
         console.log(`${fahr} is not a valid number, but is an array`);
     } else if (typeof fahr === 'object' && fahr !== null) {
